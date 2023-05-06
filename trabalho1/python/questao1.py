26d16f6512900f009babf71a3973a093e8b89ca7
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-05-06 15:29:14 -0300

    Trabalho1 - Começando

--- trabalho1/python/questao1.py
+++ trabalho1/python/questao1.py
@@ -8,24 +8,24 @@ den = [16, 3.2, 1600]  # denominador da função de transferência
 sys = ctrl.TransferFunction(num, den)  # criar o objeto que representa o sistema
 
 # 2. Definir os valores de tempo para simulação
-t = np.linspace(0, 10, 1000)  # valores de tempo de 0 a 10 segundos
+t = np.linspace(0, 4*np.pi, 10000)  # valores de tempo de 0 a 2*pi segundos
 
 # 3. Definir o sinal de entrada como o cosseno multiplicado pelo degrau unitário
 u = np.heaviside(t, 1) * np.cos(t)
 
 # 4. Realizar a simulação da resposta do sistema usando a função `control.forced_response()`
-t, y = ctrl.forced_response(sys, T=t, U=u)
+t_out, yout= ctrl.forced_response(sys, T=t, U=u)
 
-# 5. Plotar o gráfico usando a função `matplotlib.pyplot.plot()`
-plt.plot(t, y)  # plotar o gráfico da resposta ao cosseno multiplicado pelo degrau unitário
+# 5. Calcular a frequência da resposta
+Y = np.fft.fft(yout)
+mag = np.abs(Y)
+idx = np.argmax(mag)
+freq = np.fft.fftfreq(len(t), t[1]-t[0])[idx]
+print('A frequência da resposta é:', freq, 'rad/s')
 
-# 6. Adicionar rótulos aos eixos e título ao gráfico
-plt.xlabel('Tempo (s)')  # definir o rótulo do eixo x
-plt.ylabel('Saída do sistema')  # definir o rótulo do eixo y
-plt.title('Resposta ao cosseno multiplicado pelo degrau unitário')  # definir o título do gráfico
-
-# 7. Adicionar uma grade ao gráfico
-plt.grid()  # adicionar a grade ao gráfico
-
-# 8. Exibir o gráfico
+# 6. Plotar o gráfico da resposta
+plt.plot(t_out, yout)
+plt.xlabel('t')
+plt.ylabel('y(t)')
+plt.title('Resposta ao cosseno multiplicado pelo degrau unitário')
 plt.show()
