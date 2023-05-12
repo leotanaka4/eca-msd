87f1c421ea81a1ecf9f348cfd78253583e47019d
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-05-11 22:23:46 -0300

    Trabalho1 - Finalizando com os gráficos

--- trabalho1/python/questao4.py
+++ trabalho1/python/questao4.py
@@ -16,8 +16,12 @@ u = np.heaviside(t, 1) * np.cos(100*t)
 # 4. Realizar a simulacao da resposta do sistema usando a funcao `control.forced_response()`
 t_out, yout = ctrl.forced_response(sys, T=t, U=u)
 
-# 5. Plotar o grafico da resposta
+# 5. Resposta analitica
+y_a = 100/16*(-0.0085*np.exp(-0.1*t)*np.cos(9.999*t)+0.0010*np.exp(-0.1*t)*np.sin(9.999*t) + 1.0085*np.cos(100*t) - 0.0002*np.sin(100*t))
+
+# 6. Plotar o grafico da resposta
 plt.plot(t_out, yout)
+plt.plot(t, y_a)
 plt.xlabel('t')
 plt.ylabel('y(t)')
 plt.grid()
