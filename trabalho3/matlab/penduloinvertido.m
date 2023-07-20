bf6b059fbd782aedfd994ae3fcc0e697bea33933
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-07-19 22:05:17 -0300

    Trabalho3 - Não consegui usar o simulink

--- trabalho3/matlab/penduloinvertido.m
+++ trabalho3/matlab/penduloinvertido.m
@@ -1,6 +1,6 @@
 function [sys,x0,str,ts] = penduloinvertido(t,x,flag)
     switch flag
-        case 0 % initialize
+        case 0
             str = [] ;
             ts = [0 0] ;
             s = simsizes ;
@@ -9,19 +9,16 @@ function [sys,x0,str,ts] = penduloinvertido(t,x,flag)
             s.NumOutputs = 6 ;
             s.NumInputs = 2 ;
             s.DirFeedthrough = 0 ;
-            s.NumSampleTimes = 6 ;
+            s.NumSampleTimes = 1 ;
             sys = simsizes(s) ;
             x0 = [0 0 0 0 0 0] ;
-        case 1 % derivatives
+        case 1
             sys = penduloinvertido_f(t,x) ;
-        case 3 % output
+        case 3
             sys = x;
-        case {2 4 9} % 2:discrete
-                    % 4:calcTimeHit
-                    % 9:termination
+        case {2 4 9}
             sys =[];
         otherwise
             error(['unhandled flag =',num2str(flag)]) ;
     end
 end
-
--- trabalho3/matlab/penduloinvertido_f.m
+++ trabalho3/matlab/penduloinvertido_f.m
@@ -3,12 +3,10 @@ function dx = penduloinvertido_f(t, x)
     theta = x(1);
     psi = x(2);
     phi = x(3);
-
     % Derivadas das variaveis de estado
     theta_dot = x(4);
     psi_dot = x(5);
     phi_dot = x(6);
-
     % Parametros do sistema
     g = 9.8;  % gravidade
     m = 0.03; % peso da roda
@@ -26,40 +24,33 @@ function dx = penduloinvertido_f(t, x)
     Ke = 0.4; % e constante de EMF (V s/rad)
     fm = 0.0022; % atrito entre o corpo e o motor
     n = 1; % reducao do motor
-
     % Matriz de inercia inversa
     M_inv = inv([
         R^2 * (2 * m + M) + 2 * Jw, M * R * L * cos(psi), 0;
         M * R * L * cos(psi), M * L^2 + Jpsi, 0;
         0, 0, m * (W^2 / 2) + M * L * sin(psi)^2 + Jphi + (W^2 / (2 * R^2)) * Jw
     ]);
-
     % Matriz G
     G = [
         0;
         -M * g * L * sin(psi);
         0
     ];
-
     % Matriz C
     C = [
         -M * R * L * sin(psi) * psi_dot^2;
         -M * L^2 * sin(psi) * cos(psi) * phi_dot^2;
         M * L^2 * sin(2 * psi) * psi_dot * phi_dot
     ];
-
     % Matriz Q
     Q = [
         -2 * ((n * Kt * Ke / Rm) + fm), 2 * ((n * Kt * Ke / Rm) + fm), 0;
         2 * ((n * Kt * Ke / Rm) + fm), -2 * ((n * Kt * Ke / Rm) + fm), 0;
         0, 0, -W^2 / (2 * R^2) * ((n * Kt * Ke / Rm) + fm)
     ];
-
     F = Q*[theta_dot; psi_dot; phi_dot]
-
     % Vetor de entrada
     V = [0; 0]; % [V_l; V_r]
-
     % Sistema de equacoes diferenciais
     dx = zeros(6, 1);
     dx(1:3) = [theta_dot; psi_dot; phi_dot];
